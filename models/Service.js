843cd943a7bb91ecd79540c2d2290874742cae5b
Berat Arslan <arslanberattdev@gmail.com>
2025-08-17 17:06:29 +0300

    Hizmetlere related yazılar eklendi

--- models/Service.js
+++ models/Service.js
@@ -3,11 +3,7 @@ const mongoose = require("mongoose");
 const ServiceSchema = new mongoose.Schema({
   title: { type: String, required: true }, // Örn: Online Terapi
   description: { type: String, default: "" }, // Detay açıklama (varsa)
-  related: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "BlogPost",
-    required: true,
-  },
+  related: { type: String, required: true },
 });
 
 module.exports = mongoose.model("Service", ServiceSchema);
