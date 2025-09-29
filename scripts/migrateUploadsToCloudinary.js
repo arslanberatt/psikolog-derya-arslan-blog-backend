1613d247648fcc26de41b20a39f0e2551f6a61c9
Berat Arslan <arslanberattdev@gmail.com>
2025-09-29 23:01:42 +0300

    chore: save local changes before merging to main

--- scripts/migrateUploadsToCloudinary.js
+++ scripts/migrateUploadsToCloudinary.js
@@ -21,14 +21,18 @@ async function migrateField(Model, fieldName, query = {}) {
   const regex = /\/uploads\//i;
   const filter = { [fieldName]: { $regex: regex } };
   const docs = await Model.find({ ...query, ...filter });
-  console.log(`\n[${Model.modelName}] Found ${docs.length} documents to migrate for field '${fieldName}'.`);
+  console.log(
+    `\n[${Model.modelName}] Found ${docs.length} documents to migrate for field '${fieldName}'.`
+  );
   let successCount = 0;
   let failCount = 0;
 
   for (const doc of docs) {
     const originalUrl = doc[fieldName];
     if (!originalUrl) continue;
-    const filename = extractFilenameFromUploadsUrl(originalUrl) || `${Model.modelName.toLowerCase()}_${doc._id}`;
+    const filename =
+      extractFilenameFromUploadsUrl(originalUrl) ||
+      `${Model.modelName.toLowerCase()}_${doc._id}`;
     const publicIdBase = filename.replace(/\.[^.]+$/, "");
 
     try {
@@ -47,7 +51,9 @@ async function migrateField(Model, fieldName, query = {}) {
     }
   }
 
-  console.log(`[${Model.modelName}] Migration done for '${fieldName}'. Success: ${successCount}, Fail: ${failCount}.`);
+  console.log(
+    `[${Model.modelName}] Migration done for '${fieldName}'. Success: ${successCount}, Fail: ${failCount}.`
+  );
 }
 
 (async () => {
@@ -66,5 +72,3 @@ async function migrateField(Model, fieldName, query = {}) {
     process.exit(1);
   }
 })();
-
-
